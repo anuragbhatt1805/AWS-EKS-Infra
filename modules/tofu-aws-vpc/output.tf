7f6c0918bd086b5dc903f97fcd9d2c33bdcbea37
Anurag Bhatt <anuragbhatt1805@gmail.com>
2026-09-19 23:02:30 +0530

    feat: add outputs for public and private route table IDs in VPC module

--- modules/tofu-aws-vpc/output.tf
+++ modules/tofu-aws-vpc/output.tf
@@ -13,7 +13,17 @@ output "public_subnet_ids" {
   value       = aws_subnet.public[*].id
 }
 
+output "public_route_table_ids" {
+  description = "List of public route table IDs"
+  value       = aws_route_table.public[*].id
+}
+
 output "private_subnet_ids" {
   description = "List of private subnet IDs"
   value       = aws_subnet.private[*].id
+}
+
+output "private_route_table_ids" {
+  description = "List of private route table IDs"
+  value       = aws_route_table.private[*].id
 }
