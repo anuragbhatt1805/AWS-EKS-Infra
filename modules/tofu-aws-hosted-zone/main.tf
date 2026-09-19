1105c84a387eca9eb202ba887d94b597d9781c7d
Anurag Bhatt <anuragbhatt1805@gmail.com>
2026-09-20 00:06:50 +0530

    feat: add Cloudflare support with hosted zone modules and API token integration

---
+++ modules/tofu-aws-hosted-zone/main.tf
@@ -0,0 +1,56 @@
+locals {
+  hosted_zone_name = trimsuffix(var.hosted_zone_name, ".")
+
+  domain_parts = split(".", local.hosted_zone_name)
+
+  main_domain = join(
+    ".",
+    slice(
+      local.domain_parts,
+      length(local.domain_parts) - 2,
+      length(local.domain_parts)
+    )
+  )
+}
+
+data "aws_region" "current" {}
+
+data "cloudflare_zone" "public" {
+  filter = {
+    name = local.main_domain
+  }
+}
+
+resource "aws_route53_zone" "public" {
+  name = var.hosted_zone_name
+
+  lifecycle {
+    enabled = !var.private_hosted_zone
+  }
+}
+
+resource "aws_route53_zone" "private" {
+  name = var.hosted_zone_name
+
+  vpc {
+    vpc_id = var.vpc_id
+  }
+
+  lifecycle {
+    enabled = var.private_hosted_zone
+  }
+}
+
+resource "cloudflare_record" "public" {
+    for_each = toset(aws_route53_zone.public.name_servers)
+
+  zone_id = data.cloudflare_zone.public.id
+  name    = var.hosted_zone_name
+  value   = each.value
+  type    = "NS"
+  ttl     = 300
+
+  lifecycle {
+    enabled = var.enable_cloudflare && !var.private_hosted_zone
+  }
+}
--- src/core/variable.tf
+++ src/core/variable.tf
@@ -9,3 +9,21 @@ variable "aws_availability_zones" {
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
   description = "Default Availability Zones for AWS Provider Setup"
 }
+
+variable "cloudflare_api_token" {
+  description = "Cloudflare API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "public_hosted_zone_name" {
+  type        = string
+  description = "The name of the public hosted zone"
+  default     = null
+}
+
+variable "private_hosted_zone_name" {
+  type        = string
+  description = "The name of the private hosted zone"
+  default     = null
+}
