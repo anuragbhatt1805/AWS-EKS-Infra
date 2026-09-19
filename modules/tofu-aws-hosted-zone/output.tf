a2dba99fe1fc8256fa87720db19bac854a623d0a
Anurag Bhatt <anuragbhatt1805@gmail.com>
2026-09-20 00:22:53 +0530

    fix: correct aws_region reference in VPC endpoint service name and update output references in hosted zone module

--- modules/tofu-aws-hosted-zone/output.tf
+++ modules/tofu-aws-hosted-zone/output.tf
@@ -1,7 +1,7 @@
 output "public_zone_id" {
-  value = var.private_hosted_zone ? null : module.aws_route53_zone.public.zone_id
+  value = var.private_hosted_zone ? null : aws_route53_zone.public.zone_id
 }
 
 output "private_zone_id" {
-  value = var.private_hosted_zone ? module.aws_route53_zone.private.zone_id : null
+  value = var.private_hosted_zone ? aws_route53_zone.private.zone_id : null
 }
--- modules/tofu-aws-vpc-endpoint/main.tf
+++ modules/tofu-aws-vpc-endpoint/main.tf
@@ -2,7 +2,7 @@ data "aws_region" "current" {}
 
 resource "aws_vpc_endpoint" "this" {
   vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.${var.service_name}"
+  service_name      = "com.amazonaws.${data.aws_region.current.region}.${var.service_name}"
   vpc_endpoint_type = var.endpoint_type
 
   route_table_ids = var.route_table_ids
