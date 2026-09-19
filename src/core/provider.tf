98aa2ec9231cfc33d7fd55fab4dcb9f4c9566032
Anurag Bhatt <anuragbhatt1805@gmail.com>
2026-09-19 19:29:22 +0530

    chore: refactor Terraform configurations for improved backend management and variable handling

---
+++ src/core/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project    = "AWS-EKS"
+      ManagedVia = "Github"
+      Owner      = "Anurag Bhatt"
+      Module     = "Core"
+    }
+  }
+}
---
+++ src/core/variable.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "Default Region for AWS Provider Setup"
+}
